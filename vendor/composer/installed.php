5a055251929c907d019b38566ee042c5c4e7c26d
AdrianDave17 <201980134@psu.palawan.edu.ph>
2022-03-19 00:01:30 +0800

    changes made

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '654f80f5675d8688bd7042302b86e8a5a6a997df',
+        'name' => 'adriandave17/logapp-convocar',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'adriandave17/logapp-convocar' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '654f80f5675d8688bd7042302b86e8a5a6a997df',
+            'dev_requirement' => false,
+        ),
+    ),
+);
