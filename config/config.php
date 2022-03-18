015b1a9f943ae5182597daecb67c29a663bf41fe
AdrianDave17 <201980134@psu.palawan.edu.ph>
2022-03-18 22:09:21 +0800

    changes made

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-	define('ROOT_URL', 'https://immense-chamber-23349.herokuapp.com/');
+	define('ROOT_URL', '');
 	define('DB_HOST', 'sql6.freesqldatabase.com');
 	define('DB_USER', 'sql6479706');
 	define('DB_PASS', '8a5dIBG8ks');
