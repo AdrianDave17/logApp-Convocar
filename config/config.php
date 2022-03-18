ada539053c8bae2fd11a9e78320cbed2c231cab9
AdrianDave17 <201980134@psu.palawan.edu.ph>
2022-03-18 21:42:05 +0800

    updated

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+	define('ROOT_URL', '');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6479706');
+	define('DB_PASS', '8a5dIBG8ks');
+	define('DB_NAME', 'sql6479706');
+	
+?>
