b958632ef6c70c9892cd1647e5aa89cc18c50405
AdrianDave17 <201980134@psu.palawan.edu.ph>
2022-03-19 10:47:58 +0800

    changes made

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7cbe421f4c515eeac6ef17d12c2a8f0b4c2b1c40',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7cbe421f4c515eeac6ef17d12c2a8f0b4c2b1c40',
+            'dev_requirement' => false,
+        ),
+    ),
+);
